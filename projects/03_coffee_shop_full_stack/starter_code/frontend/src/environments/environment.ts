3f2863e1770787351f3d5c58d860ff3b51bc3227
Rashwan <Ahmed.Rashwan2014@yahoo.com>
2020-10-06 20:13:05 +0200

    Update Environment variables

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,10 +1,6 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'http://localhost:5000', // the running FLASK api server url
   auth0: {
     url: 'arashwan.us', // the auth0 domain prefix
     audience: 'http://127.0.0.1:5000', // the audience set for the auth0 app
